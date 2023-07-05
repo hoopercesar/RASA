2812bb60169b3a07b04b3d7d420b1d15fa855e5b
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-07-05 17:37:00 -0400

    Add files via upload

--- pizzaFormulario/actions/actions.py
+++ pizzaFormulario/actions/actions.py
@@ -1,27 +1,47 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker, FormValidationAction
+from rasa_sdk.events import EventType
+from rasa_sdk.types import DomainDict
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+tamanos_de_pizza = ['grande', 'mediana', 'mediano', 'pequeña', 'chica', 'pequeño']
+tipos_de_pizza = ['vegana', 'chocolate', 'calabresa', 'frutillas', "queso_catupiry"]
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ValidateSimplePizza(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_simple_pizza_form"
+    
+    def validate_pizza_size(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate pizza_size value"""
+
+        if slot_value.lower() not in tamanos_de_pizza:
+            dispatcher.utter_message(text=f"Sólo tenemos pizza grande, meddiana o pequeña")
+            return {"pizza_size": None}
+        dispatcher.utter_message(text=f"Perfecto, has solicitado una pizza {slot_value}")
+        return {"pizza_size": slot_value}
+    
+    def validate_pizza_type(
+            self,
+            slot_value: Any,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict, 
+    ) -> Dict[Text, Any]:
+        """Validate pizza_type value"""
+
+        if slot_value.lower() not in tipos_de_pizza:
+            dispatcher.utter_message(text=f"Sólo tenemos pizza vegana, de chocolate, calabresa, frutillas, chocolate y queso_catupiry")
+            return {"pizza_type": None}
+        dispatcher.utter_message(text=f"Ok, has elegido la pizza {slot_value}")
+        return {"pizza_type": slot_value}
+
+
+

963e1391b40744da23966cf4d57d8a1ebc248164
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-07-08 22:38:39 -0400

    Add files via upload

--- Connect2DataBase/actions/test.py
+++ Connect2DataBase/actions/test.py
@@ -1,13 +1,26 @@
-import sqlite3
-import os
-import pandas as pd
-import numpy as np
+# import sqlite3
+# import os
+# import pandas as pd
+# import numpy as np
 
-path = 'C:/Users/Cesar Hooper/Documents/STARTUP/dataset_estudio.db'
-con = sqlite3.connect(path, check_same_thread=False)
-cur = con.cursor()
+# ruts = ['14859287-k', '10533064-2', '11437936-0', '18218988-k', '15368887-7']
 
-cur.execute("SELECT rut FROM dataset_estudio")
-rows = cur.fetchall()
+# path = 'C:/Users/Cesar Hooper/Documents/STARTUP/dataset_estudio.db'
+# con = sqlite3.connect(path, check_same_thread=False)
+# cur = con.cursor()
+# rut = '15368887-7'
 
-print(rows)
+# cur.execute("SELECT * FROM dataset_estudio WHERE rut=?", (rut, ))
+# rows = cur.fetchall()
+# lista = []
+# for row in rows:
+#     lista.append(row[0])
+
+# cur.execute("SELECT * FROM dataset_estudio")
+# datos = cur.fetchall()
+
+# k = 0 
+# for dato in datos:
+#     if rut == : 
+#        print(k, dato)
+#     k += 1

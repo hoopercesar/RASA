1cb02c5cc95e2ae5e4cfe2aa1628ee2dde1bef4f
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-07-19 23:01:14 -0400

    Add files via upload

--- healthCareBot/actions/actions.py
+++ healthCareBot/actions/actions.py
@@ -6,11 +6,28 @@ from rasa_sdk.events import EventType
 from rasa_sdk.types import DomainDict
 from rasa_sdk.executor import CollectingDispatcher
 import sqlite3
+import hashlib
+import json
+
 
 class ValidateRut(FormValidationAction):
     def name(self) -> Text:
        return "validate_rut_form"
     
+    # función para hashear datos
+    @staticmethod
+    def generar_hash(cadena):
+        # Crear un objeto de hash utilizando el algoritmo SHA256
+        hasher = hashlib.sha256()
+
+        # Convertir la cadena en bytes y actualizar el objeto de hash
+        hasher.update(cadena.encode('utf-8'))
+
+        # Obtener el hash resultante en formato hexadecimal
+        hash_resultado = hasher.hexdigest()
+
+        return hash_resultado
+
     # entrega lista de keys de diccionarios
     @staticmethod
     def keylist(dicc):
@@ -18,31 +35,25 @@ class ValidateRut(FormValidationAction):
         for k in dicc:
             keylist.append(k)
     
-        return keylist
+        return keylist   
         
-
+    # retorna lista con ruts hasheados dentro de DB
     @staticmethod
-    def rut_db() -> List[Text]:
+    def rut_db(self, rut) -> List[Text]:
         """ruts database"""
         path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
         con = sqlite3.connect(path, check_same_thread=False)
         cur = con.cursor()
         cur.execute("SELECT * FROM datospersonales")
-        rows = cur.fetchall()
-        lista = []
-        for row in rows:
-            lista.append(row[0])
+        datos = cur.fetchall()
 
-        return lista
-
-    @staticmethod
-    def get_user_info(userRut) -> tuple[Text]:
-        path = 'C:/Users/Cesar Hooper/Documents/STARTUP/dataset_estudio.db'
-        con = sqlite3.connect(path, check_same_thread=False)
-        cur = con.cursor()
-        cur.execute("SELECT * FROM dataset_estudio WHERE rut=?", (userRut, ))
-        userInfo = cur.fetchall()
-        return userInfo
+        paciente = None
+        hashedRut = self.generar_hash(rut)
+        for dato in datos:
+            dat = json.loads(dato[0])
+            if (hashedRut == self.keylist(dat)[0]):
+                paciente = dat[hashedRut]
+                return paciente
 
     def validate_rut(                  
         self,
@@ -54,10 +65,11 @@ class ValidateRut(FormValidationAction):
                 
         """Validate rut value"""
 
-        if slot_value not in self.rut_db(): 
-            dispatcher.utter_message(text=f"{slot_value} no existe")
+        if (self.rut_db(self, slot_value) ==  None): 
+            dispatcher.utter_message(text=f"{slot_value} Rut no Valido")
             return {'rut': None}
         else:
-            usuario = self.get_user_info(slot_value)
-            dispatcher.utter_message(text=f"{slot_value} es un usuario activo")
-            return {'rut': usuario}
+            usuario = self.rut_db(self, slot_value)
+            nombre = usuario['nombre']
+            dispatcher.utter_message(template="utter_rut_slots", nombre=nombre)
+            return []

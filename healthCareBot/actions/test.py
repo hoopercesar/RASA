50e057a140b476b2681e5c96dcd799f0410e3c0d
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-07-26 22:26:45 -0400

    Add files via upload

--- healthCareBot/actions/test.py
+++ healthCareBot/actions/test.py
@@ -1,167 +1,190 @@
-import sqlite3
-import hashlib
-import json
+# import sqlite3
+# import hashlib
+# import json
 
-class ValidateRut():
-    def __init__(self, rut):
-        self.rut = rut
+# class ValidateRut():
+#     def __init__(self, rut):
+#         self.rut = rut
     
-    # función para hashear datos
-    @staticmethod
-    def generar_hash(cadena):
-        # Crear un objeto de hash utilizando el algoritmo SHA256
-        hasher = hashlib.sha256()
+#     # función para hashear datos
+#     @staticmethod
+#     def generar_hash(cadena):
+#         # Crear un objeto de hash utilizando el algoritmo SHA256
+#         hasher = hashlib.sha256()
 
-        # Convertir la cadena en bytes y actualizar el objeto de hash
-        hasher.update(cadena.encode('utf-8'))
+#         # Convertir la cadena en bytes y actualizar el objeto de hash
+#         hasher.update(cadena.encode('utf-8'))
 
-        # Obtener el hash resultante en formato hexadecimal
-        hash_resultado = hasher.hexdigest()
+#         # Obtener el hash resultante en formato hexadecimal
+#         hash_resultado = hasher.hexdigest()
 
-        return hash_resultado
+#         return hash_resultado
 
-    # entrega lista de keys de diccionarios
-    @staticmethod
-    def keylist(dicc):
-        keylist = []
-        for k in dicc:
-            keylist.append(k)    
-        return keylist   
+#     # entrega lista de keys de diccionarios
+#     @staticmethod
+#     def keylist(dicc):
+#         keylist = []
+#         for k in dicc:
+#             keylist.append(k)    
+#         return keylist   
         
-    # retorna lista con ruts hasheados dentro de DB
-    @staticmethod
-    def rut_db(self, rut):
-        """ruts database"""
-        path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
-        con = sqlite3.connect(path, check_same_thread=False)
-        cur = con.cursor()
-        cur.execute("SELECT * FROM datospersonales")
-        datos = cur.fetchall()
+#     # retorna lista con ruts hasheados dentro de DB
+#     @staticmethod
+#     def rut_db(self, rut):
+#         """ruts database"""
+#         path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
+#         con = sqlite3.connect(path, check_same_thread=False)
+#         cur = con.cursor()
+#         cur.execute("SELECT * FROM datospersonales")
+#         datos = cur.fetchall()
 
-        paciente = None
-        hashedRut = self.generar_hash(rut)
-        for dato in datos:
-            dat = json.loads(dato[0])
-            if (hashedRut == self.keylist(dat)[0]):
-                paciente = dat[hashedRut]
-                # print('PACIENTE', paciente['userID'])
-                return paciente
+#         paciente = None
+#         hashedRut = self.generar_hash(rut)
+#         for dato in datos:
+#             dat = json.loads(dato[0])
+#             if (hashedRut == self.keylist(dat)[0]):
+#                 paciente = dat[hashedRut]
+#                 # print('PACIENTE', paciente['userID'])
+#                 return paciente
 
         
                 
-    @staticmethod
-    def get_user_info(userRut):
-        path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
-        con = sqlite3.connect(path, check_same_thread=False)
-        cur = con.cursor()
-        cur.execute("SELECT * FROM dataset_estudio WHERE rut=?", (userRut, ))
-        userInfo = cur.fetchall()
-        return userInfo
+#     @staticmethod
+#     def get_user_info(userRut):
+#         path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
+#         con = sqlite3.connect(path, check_same_thread=False)
+#         cur = con.cursor()
+#         cur.execute("SELECT * FROM dataset_estudio WHERE rut=?", (userRut, ))
+#         userInfo = cur.fetchall()
+#         return userInfo
 
-    def validate_rut(self):
+#     def validate_rut(self):
                 
-        """Validate rut value"""
-        mensaje = ''
-        if (self.rut_db(self, self.rut) ==  None):
-            mensaje = 'Rut no válido'
-            return mensaje
-        else:
-            return self.rut_db(self, self.rut)
+#         """Validate rut value"""
+#         mensaje = ''
+#         if (self.rut_db(self, self.rut) ==  None):
+#             mensaje = 'Rut no válido'
+#             return mensaje
+#         else:
+#             return self.rut_db(self, self.rut)
 
-#### Clase diagnóstico y tratamiento #####
+# #### Clase diagnóstico y tratamiento #####
 
-class Tratamiento(ValidateRut):
-    def __init__(self, rut):
+# class Tratamiento(ValidateRut):
+#     def __init__(self, rut):
   
-        self.rut = rut
+#         self.rut = rut
 
-    path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
-    con = sqlite3.connect(path, check_same_thread=False)
-    cur = con.cursor()
+#     path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
+#     con = sqlite3.connect(path, check_same_thread=False)
+#     cur = con.cursor()
 
-    @staticmethod
-    def keylist(dicc):
-        keylist = []
-        for k in dicc:
-            keylist.append(k)    
-        return keylist   
+#     @staticmethod
+#     def keylist(dicc):
+#         keylist = []
+#         for k in dicc:
+#             keylist.append(k)    
+#         return keylist   
     
-    # cur.execute("SELECT diagnostico FROM tratamiento WHERE userID=?", (userID, ))
-    # userInfo = cur.fetchall()
-    @staticmethod
-    def userID(rut):
-        val = ValidateRut(rut)
-        # print('AQUI ESTA', val.rut_db(rut))
-        return val.validate_rut()['userID']
+#     # cur.execute("SELECT diagnostico FROM tratamiento WHERE userID=?", (userID, ))
+#     # userInfo = cur.fetchall()
+#     @staticmethod
+#     def userID(rut):
+#         val = ValidateRut(rut)
+#         # print('AQUI ESTA', val.rut_db(rut))
+#         return val.validate_rut()['userID']
 
-    def diagnosticos(self):
-        userID = self.userID(self.rut)
-        path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
-        con = sqlite3.connect(path, check_same_thread=False)
-        cur = con.cursor()
-        cur.execute("SELECT diagnostico FROM tratamiento WHERE userID=?", (userID, ))
-        userInfo = json.loads(cur.fetchall()[0][0])
+#     def diagnosticos(self):
+#         userID = self.userID(self.rut)
+#         path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
+#         con = sqlite3.connect(path, check_same_thread=False)
+#         cur = con.cursor()
+#         cur.execute("SELECT diagnostico FROM tratamiento WHERE userID=?", (userID, ))
+#         userInfo = json.loads(cur.fetchall()[0][0])
 
-        diagnosticos = self.keylist(userInfo)
-        print('DIAGNOSTICOS', diagnosticos[0])
+#         diagnosticos = self.keylist(userInfo)
+        
 
-        return diagnosticos
+#         return diagnosticos
 
-    def medicamentos(self):
-        userID = self.userID(self.rut)
-        path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
-        con = sqlite3.connect(path, check_same_thread=False)
-        cur = con.cursor()
-        cur.execute("SELECT diagnostico FROM tratamiento WHERE userID=?", (userID, ))
-        userInfo = json.loads(cur.fetchall()[0][0])
+#     def medicamentos(self):
+#         userID = self.userID(self.rut)
+#         path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
+#         con = sqlite3.connect(path, check_same_thread=False)
+#         cur = con.cursor()
+#         cur.execute("SELECT diagnostico FROM tratamiento WHERE userID=?", (userID, ))
+#         userInfo = json.loads(cur.fetchall()[0][0])
 
         
-        dd = {}
-        for diag in self.diagnosticos():
-            # print(diag, userInfo[diag])
-            dd[diag] = self.keylist(userInfo[diag])
+#         dd = {}
+#         for diag in self.diagnosticos():
+#             # print(diag, userInfo[diag])
+#             dd[diag] = self.keylist(userInfo[diag])
 
-        ## IDEA: CUANDO PACIENTE PREGUNTE POR DIAGNÓSTIO
-        ## EL BOT RESPONDE CON EL MÉTODO DIAGNÓSTICO: UNA LISTA DE LAS ENFERMEDADES...
-        ## CUANDO PACIENTE PREGUNTA POR MEDICAMENTOS, SE ENTREGA LA ENFERMEDAD
-        ## JUNTO CON LA LISTA DE LOS MEDICAMENTOS PARA EL TRATAMIENTO. 
-        return dd
+#         ## IDEA: CUANDO PACIENTE PREGUNTE POR DIAGNÓSTIO
+#         ## EL BOT RESPONDE CON EL MÉTODO DIAGNÓSTICO: UNA LISTA DE LAS ENFERMEDADES...
+#         ## CUANDO PACIENTE PREGUNTA POR MEDICAMENTOS, SE ENTREGA LA ENFERMEDAD
+#         ## JUNTO CON LA LISTA DE LOS MEDICAMENTOS PARA EL TRATAMIENTO. 
+
+#         # salida de los medicamentos: 'enfermedad y los medicamentos indicados por el médico'
+#         # enfs lista de enfrmedades
+#         enfs = self.keylist(dd)
+#         textos = []
+#         for enf in enfs:
+#             textos.append(' Para el cuadro de ' + enf.upper() + ' el tratamiento es con: ' + ', '.join([k for k in dd[enf]]) + '.')
+#             # print('Para el cuadro de ' + enf.upper() + ' el tratamiento es con: ' + ', '.join([k for k in dd[enf]]))
+
+#         texto = '\n '.join([tex for tex in textos])
+#         print(texto)
+
+#         return dd
     
-    def dosis(self):
-        userID = self.userID(self.rut)
-        path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
-        con = sqlite3.connect(path, check_same_thread=False)
-        cur = con.cursor()
-        cur.execute("SELECT diagnostico FROM tratamiento WHERE userID=?", (userID, ))
-        userInfo = json.loads(cur.fetchall()[0][0])
+#     def dosis(self):
+#         userID = self.userID(self.rut)
+#         path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
+#         con = sqlite3.connect(path, check_same_thread=False)
+#         cur = con.cursor()
+#         cur.execute("SELECT diagnostico FROM tratamiento WHERE userID=?", (userID, ))
+#         userInfo = json.loads(cur.fetchall()[0][0])
         
-        # self.keylist(userInfo) es la lista de diccionarios de cada medicina con sus dosis
-        # se extrae cada diccionario de medicina (con sus dosis) y se guarda en una sola lista
-        lista = []
-        for diag in self.keylist(userInfo):
-            lista.append(userInfo[diag])
+#         # self.keylist(userInfo) es la lista de diccionarios de cada medicina con sus dosis
+#         # se extrae cada diccionario de medicina (con sus dosis) y se guarda en una sola lista
+#         lista = []
+#         for diag in self.keylist(userInfo):
+#             lista.append(userInfo[diag])
 
-        # lista contiene los diccionarios de las medicinas con sus dosis
-        # ahora, vamor a unir todos esos diccionarios en un solo diccionario dic_final
-        dic_final = {}
-        for dic1 in lista:
-            for sub in dic1:
-                dic_final[sub] = dic1[sub]     
+#         # lista contiene los diccionarios de las medicinas con sus dosis
+#         # ahora, vamor a unir todos esos diccionarios en un solo diccionario dic_final
+#         dic_final = {}
+#         for dic1 in lista:
+#             for sub in dic1:
+#                 dic_final[sub] = dic1[sub]     
             
-        return dic_final
+#         return dic_final
 
-    def presentacionDosis(self):
-        '''en el método dosis se extraen las medicinas con sus dosis y se ordenan
-        dentro de un diccionario. sin embargo, esta presentación no es adecuada para el paciente.
-        en este método se presenta la información de manera entendible para el paciente.
-        un mensaje con el nombre de la medicina y la dosis diaria'''
+#     def presentacionDosis(self):
+#         '''en el método dosis se extraen las medicinas con sus dosis y se ordenan
+#         dentro de un diccionario. sin embargo, esta presentación no es adecuada para el paciente.
+#         en este método se presenta la información de manera entendible para el paciente.
+#         un mensaje con el nombre de la medicina y la dosis diaria'''
+#         dosis = self.dosis()
+#         lista = self.keylist(dosis)
+#         mensajes = []
+#         for li in lista:
+#             mensajes.append(dosis[li][0] + ' de ' + li.upper()  + '. ' 
+#                             + str(dosis[li][1]) + ' dosis al dia. Durante ' + str(dosis[li][2]) + ' dias.' + '\n')           
+#             # print(li, dosis[li])
+
+#         texto = ' '.join([mensaje for mensaje in mensajes])
+
+#         # print(texto)
         
-        return 'algo'
+#         return texto
         
-# fran = 12658439-3
-# amore = 20502458-1
+# # fran = 12658439-3
+# # amore = 20502458-1
         
-t = Tratamiento('20502458-1')      
-v = ValidateRut('13109915-0')   
-m = {'key uno': 18}
-print(t.diagnosticos(), m['key uno'])
+# t = Tratamiento('13109915-0')      
+# v = ValidateRut('13109915-0')   
+# m = {'key uno': 18}
+# print(t.medicamentos())

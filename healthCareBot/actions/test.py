5f5cb6b27c4446e34b20ec0bd1caac71cda2244e
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-07-30 22:59:48 -0400

    Add files via upload

--- healthCareBot/actions/test.py
+++ healthCareBot/actions/test.py
@@ -9,7 +9,85 @@ class ValidateRut():
     def __init__(self, rut):
         self.rut = rut
 
-    # entrega fecha y hora actual en formato 'year-' 
+
+
+
+    
+    # función para hashear datos
+    @staticmethod
+    def generar_hash(cadena):
+        # Crear un objeto de hash utilizando el algoritmo SHA256
+        hasher = hashlib.sha256()
+
+        # Convertir la cadena en bytes y actualizar el objeto de hash
+        hasher.update(cadena.encode('utf-8'))
+
+        # Obtener el hash resultante en formato hexadecimal
+        hash_resultado = hasher.hexdigest()
+
+        return hash_resultado
+
+    # entrega lista de keys de diccionarios
+    @staticmethod
+    def keylist(dicc):
+        keylist = []
+        for k in dicc:
+            keylist.append(k)    
+        return keylist   
+        
+    # retorna lista con ruts hasheados dentro de DB
+    @staticmethod
+    def rut_db(self, rut):
+        """ruts database"""
+        path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
+        con = sqlite3.connect(path, check_same_thread=False)
+        cur = con.cursor()
+        cur.execute("SELECT * FROM datospersonales")
+        datos = cur.fetchall()
+
+        paciente = None
+        hashedRut = self.generar_hash(rut)
+        for dato in datos:
+            dat = json.loads(dato[0])
+            if (hashedRut == self.keylist(dat)[0]):
+                paciente = dat[hashedRut]
+                return paciente
+
+        
+                
+    @staticmethod
+    def get_user_info(userRut):
+        path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
+        con = sqlite3.connect(path, check_same_thread=False)
+        cur = con.cursor()
+        cur.execute("SELECT * FROM dataset_estudio WHERE rut=?", (userRut, ))
+        userInfo = cur.fetchall()
+        return userInfo
+
+    def validate_rut(self):
+                
+        """Validate rut value"""
+
+        mensaje = ''
+        if (self.rut_db(self, self.rut) ==  None):
+            mensaje = 'Rut no válido'
+            return mensaje
+        else:
+            return self.rut_db(self, self.rut)
+
+#### Clase diagnóstico y tratamiento #####
+
+class Tratamiento(ValidateRut):
+    def __init__(self, rut):
+  
+        self.rut = rut
+
+    path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
+    con = sqlite3.connect(path, check_same_thread=False)
+    cur = con.cursor()
+
+
+        # entrega fecha y hora actual en formato 'year-' 
     @staticmethod
     def entregaHora():
         hora = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
@@ -20,10 +98,10 @@ class ValidateRut():
     @staticmethod
     def creaHorarios(diaInicio, horaInicio, datosMedicina):
         ''' 
-        COMMAND: creaHorarios('ahora', 'ahora', ['200mg', 8, 3])
-        RETORNA: arreglo con los horarios de medicinas en formato string 'year-'   
-        diaInicio: 'year, month, day'
-        horaInicio: 'hh:mm'
+        COMMAND: creaHorarios('fechaInicio', 'horaInicio', ['200mg', 8, 3])
+        diaInicio: string -> 'yyyy, mm, dd' (default: 'ahora' -> fecha actual)
+        horaInicio: string -> 'hh:mm' (default: 'ahora'-> hora actual)
+        RETORNA: arreglo con los horarios de medicinas en formato string 'year-month-day hh:mm'   
         datosMedicina: ['cantidadmg', frecuencia, duracion] 
         '''    
     # Configuración de inicio de las fechas de inicio y de finalización
@@ -69,92 +147,6 @@ class ValidateRut():
             counter += 1            
             
         return horarios
-
-
-    
-    # función para hashear datos
-    @staticmethod
-    def generar_hash(cadena):
-        # Crear un objeto de hash utilizando el algoritmo SHA256
-        hasher = hashlib.sha256()
-
-        # Convertir la cadena en bytes y actualizar el objeto de hash
-        hasher.update(cadena.encode('utf-8'))
-
-        # Obtener el hash resultante en formato hexadecimal
-        hash_resultado = hasher.hexdigest()
-
-        return hash_resultado
-
-    # entrega lista de keys de diccionarios
-    @staticmethod
-    def keylist(dicc):
-        keylist = []
-        for k in dicc:
-            keylist.append(k)    
-        return keylist   
-        
-    # retorna lista con ruts hasheados dentro de DB
-    @staticmethod
-    def rut_db(self, rut):
-        """ruts database"""
-        path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
-        con = sqlite3.connect(path, check_same_thread=False)
-        cur = con.cursor()
-        cur.execute("SELECT * FROM datospersonales")
-        datos = cur.fetchall()
-
-        paciente = None
-        hashedRut = self.generar_hash(rut)
-        for dato in datos:
-            dat = json.loads(dato[0])
-            if (hashedRut == self.keylist(dat)[0]):
-                paciente = dat[hashedRut]
-                # print('PACIENTE', paciente['userID'])
-                return paciente
-
-        
-                
-    @staticmethod
-    def get_user_info(userRut):
-        path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
-        con = sqlite3.connect(path, check_same_thread=False)
-        cur = con.cursor()
-        cur.execute("SELECT * FROM dataset_estudio WHERE rut=?", (userRut, ))
-        userInfo = cur.fetchall()
-        return userInfo
-
-    def validate_rut(self):
-                
-        """Validate rut value"""
-        time = self.entregaHora()
-        horarios = self.creaHorarios('2023, 07, 29', '21:50', ['100mg', 3, 3])
-        print('HORARIOS', horarios[0:10], time)
-        for t in range(0, 10):
-            hora = self.entregaHora()
-            print(t, hora)
-
-            if hora in horarios:
-                message = f"{hora} Hora de Medicina!"
-                print(message)
-
-        mensaje = ''
-        if (self.rut_db(self, self.rut) ==  None):
-            mensaje = 'Rut no válido'
-            return mensaje
-        else:
-            return self.rut_db(self, self.rut)
-
-#### Clase diagnóstico y tratamiento #####
-
-class Tratamiento(ValidateRut):
-    def __init__(self, rut):
-  
-        self.rut = rut
-
-    path = 'C:/Users/Cesar Hooper/Documents/STARTUP/datapacientes.db'
-    con = sqlite3.connect(path, check_same_thread=False)
-    cur = con.cursor()
 
     @staticmethod
     def keylist(dicc):
@@ -235,6 +227,7 @@ class Tratamiento(ValidateRut):
         dic_final = {}
         for dic1 in lista:
             for sub in dic1:
+                print(dic1[sub])
                 dic_final[sub] = dic1[sub]     
             
         return dic_final
@@ -244,6 +237,19 @@ class Tratamiento(ValidateRut):
         dentro de un diccionario. sin embargo, esta presentación no es adecuada para el paciente.
         en este método se presenta la información de manera entendible para el paciente.
         un mensaje con el nombre de la medicina y la dosis diaria'''
+
+        time = self.entregaHora()
+        horarios = self.creaHorarios('2023, 07, 29', '21:50', ['100mg', 3, 3])
+        print('HORARIOS', horarios[0:10], time)
+        for t in range(0, 2):
+            hora = self.entregaHora()
+            print(t, hora)
+
+            if hora in horarios:
+                message = f"{hora} Hora de Medicina!"
+                print(message)
+
+
         dosis = self.dosis()
         lista = self.keylist(dosis)
         mensajes = []
@@ -264,4 +270,4 @@ class Tratamiento(ValidateRut):
 t = Tratamiento('13109915-0')      
 v = ValidateRut('13109915-0')   
 m = {'key uno': 18}
-print(v.validate_rut())
+print(t.dosis())

6cb4614fad8925c492e22f9ad60d25a88a64d2b0
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-08-04 21:52:37 -0400

    Add files via upload

--- healthCareBotV2/actions/actions.py
+++ healthCareBotV2/actions/actions.py
@@ -1,10 +1,10 @@
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker, ValidationAction
-from rasa_sdk.forms import FormValidationAction
 from rasa_sdk.events import EventType, SlotSet
 from rasa_sdk.types import DomainDict
 from rasa_sdk.executor import CollectingDispatcher
+# from rasa_sdk.interfaces import Action
 import sqlite3
 import hashlib
 import json
@@ -27,13 +27,47 @@ class ActionVerifyRut(Action):
         if rut == None: 
             print('Sin información')
         else: 
+            # mensaje = nombre
             print(rut, nombre)
-            dispatcher.utter_message(response="utter_verified", rut="ALGO TEST")
-
-              
-         
 
         return [SlotSet("mensaje", nombre)]  
+    
+class ActionBuscaDataBase(Action):
+    def name(self) -> Text:
+        return "action_busca_database"
+    
+    @staticmethod
+    def keylist(dicc):
+        keylist = []
+        for k in dicc:
+            keylist.append(k)
+
+        return keylist   
+    
+    @staticmethod
+    def generar_hash(cadena):
+        # Crear un objeto de hash utilizando el algoritmo SHA256
+        hasher = hashlib.sha256()
+
+        # Convertir la cadena en bytes y actualizar el objeto de hash
+        hasher.update(cadena.encode('utf-8'))
+
+        # Obtener el hash resultante en formato hexadecimal
+        hash_resultado = hasher.hexdigest()
+
+        return hash_resultado
+    
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict):
+        
+        rut = tracker.get_slot('rut')
+        rutHashed = self.generar_hash(rut)
+        print('caquita loca', rutHashed)
+
+        return []
+
         
 
         
